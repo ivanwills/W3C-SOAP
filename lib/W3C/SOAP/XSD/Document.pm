2c2419e6e5099cc47df2d8cd5eb8003c8bbb73f4
Ivan Wills <ivan.wills@gmail.com>
2012-05-26 23:37:08 +1000

    Added actual module map, started to document

--- lib/W3C/SOAP/XSD/Document.pm
+++ lib/W3C/SOAP/XSD/Document.pm
@@ -1,7 +1,7 @@
 package W3C::SOAP::XSD::Document;
 
 # Created on: 2012-05-26 15:46:31
-# Create by:  dev
+# Create by:  Ivan Wills
 # $Id$
 # $Revision$, $HeadURL$, $Date$
 # $Revision$, $Source$, $Date$
@@ -32,45 +32,51 @@ has xml => (
     required => 1,
 );
 has xc => (
-    is       => 'ro',
-    isa      => 'XML::LibXML::XPathContext',
-    builder  => '_xc',
+    is         => 'ro',
+    isa        => 'XML::LibXML::XPathContext',
+    builder    => '_xc',
     lazy_build => 1,
 );
 has target_namespace => (
-    is       => 'rw',
-    isa      => 'Str',
-    builder  => '_target_namespace',
+    is         => 'rw',
+    isa        => 'Str',
+    builder    => '_target_namespace',
     lazy_build => 1,
 );
 has imports => (
-    is       => 'rw',
-    isa      => 'ArrayRef[W3C::SOAP::XSD::Document]',
-    builder  => '_imports',
+    is         => 'rw',
+    isa        => 'ArrayRef[W3C::SOAP::XSD::Document]',
+    builder    => '_imports',
     lazy_build => 1,
 );
 has simple_types => (
-    is       => 'rw',
-    isa      => 'ArrayRef[W3C::SOAP::XSD::Document::SimpleType]',
-    builder  => '_simple_types',
+    is         => 'rw',
+    isa        => 'ArrayRef[W3C::SOAP::XSD::Document::SimpleType]',
+    builder    => '_simple_types',
     lazy_build => 1,
 );
 has complex_types => (
-    is       => 'rw',
-    isa      => 'ArrayRef[W3C::SOAP::XSD::Document::ComplexType]',
-    builder  => '_complex_types',
+    is         => 'rw',
+    isa        => 'ArrayRef[W3C::SOAP::XSD::Document::ComplexType]',
+    builder    => '_complex_types',
     lazy_build => 1,
 );
 has elements => (
-    is       => 'rw',
-    isa      => 'ArrayRef[W3C::SOAP::XSD::Document::Element]',
-    builder  => '_elements',
+    is         => 'rw',
+    isa        => 'ArrayRef[W3C::SOAP::XSD::Document::Element]',
+    builder   => '_elements',
     lazy_build => 1,
 );
 has ns_map => (
-    is       => 'rw',
-    isa      => 'HashRef[Str]',
+    is        => 'rw',
+    isa       => 'HashRef[Str]',
     predicate => 'has_ns_map',
+);
+has ns_module_map => (
+    is        => 'rw',
+    isa       => 'HashRef[Str]',
+    default   => sub {{}},
+    predicate => 'has_ns_module_map',
 );
 
 around BUILDARGS => sub {
@@ -112,7 +118,7 @@ sub _imports {
 
     for my $import (@nodes) {
         my $location = $import->getAttribute('schemaLocation');
-        push @imports, __PACKAGE__->new( location => $location );
+        push @imports, __PACKAGE__->new( location => $location, ns_module_map => $self->ns_module_map );
     }
 
     return \@imports;
@@ -171,7 +177,6 @@ sub get_ns_uri {
             = map {$_->name =~ /^xmlns:(.*)$/; ($1 => $_->value)}
             grep { $_->name =~ /^xmlns:/ }
             $self->xml->firstChild->getAttributes;
-        warn Dumper \%map;
 
         $self->ns_map(\%map);
     }
@@ -183,8 +188,10 @@ sub get_ns_uri {
 sub get_module_base {
     my ($self, $ns) = @_;
 
-    return $ns;
-    return $self->get_ns_uri($ns);
+    die "Trying to get module mappings when none specified!\n" if !$self->has_ns_module_map;
+    die "No mapping specified for the namespace $ns!\n"        if !$self->ns_module_map->{$ns};
+
+    return $self->ns_module_map->{$ns};
 }
 
 1;
@@ -193,99 +200,51 @@ __END__
 
 =head1 NAME
 
-W3C::SOAP::XSD::Document - <One-line description of module's purpose>
+W3C::SOAP::XSD::Document - Represents a XMLSchema Document
 
 =head1 VERSION
 
 This documentation refers to W3C::SOAP::XSD::Document version 0.1.
 
-
 =head1 SYNOPSIS
 
    use W3C::SOAP::XSD::Document;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
-
+   my $xsd = W3C::SOAP::XSD::Document->new(
+        location => 'my.xsd',
+        ns_base => {
+            'http://xml.namespace.com/SomeTing.html' => 'MyApp::SomeTing',
+        },
+   );
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Takes a XMLSchema Document and makes the contents available in a convienient
+interface.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
-
 =head1 DEPENDENCIES
-
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
 
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
-
-There are no known bugs in this module.
-
-Please report problems to dev (dev@localhost).
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
 
 Patches are welcome.
 
 =head1 AUTHOR
 
-dev - (dev@localhost)
-<Author name(s)>  (<contact address>)
+Ivan Wills - (ivan.wills@gmail.com)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2012 dev (123 Timbuc Too).
+Copyright (c) 2012 Ivan Wills (14 Mullion Close Hornsby Heights NSW Australia).
 All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under
